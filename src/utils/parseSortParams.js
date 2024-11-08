ed72cbc9d5c15cfb05c8101cf58b55c1e7542bc4
StIgorek <elitelifegroup@gmail.com>
2024-11-08 15:32:54 +0200

    add sortBy

---
+++ src/utils/parseSortParams.js
@@ -0,0 +1,10 @@
+const sortOrderList = ["asc", "desc"];
+
+export const parseSortParams = ({ sortBy, sortOrder }, sortByList) => {
+  const parsedSortOrder = sortOrderList.includes(sortOrder) ? sortOrder : sortOrderList[0];
+  const parsedSortBy = sortByList.includes(sortBy) ? sortBy : "_id";
+  return {
+    sortBy: parsedSortOrder,
+    sortOrder: parsedSortBy,
+  };
+};
