ea0b3eec9101102cd46a0d758510e39bf663108c
StIgorek <elitelifegroup@gmail.com>
2024-12-06 23:49:48 +0200

    add swagger

--- src/constants/index.js
+++ src/constants/index.js
@@ -7,3 +7,5 @@ export const TEMPLATE_DIR = path.resolve("src", "templates");
 export const TEMP_UPLOAD_DIR = path.resolve("temp");
 
 export const UPLOADS_DIR = path.resolve("uploads");
+
+export const SWAGGER_PATH = path.join(process.cwd(), 'docs', 'swagger.json');
